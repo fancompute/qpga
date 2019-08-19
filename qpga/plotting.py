ede4802ffb434fac992a9464ea5b22dabc6b4d40
Ben Bartlett <benjamincbartlett@gmail.com>
2019-08-19 12:12:42 -0700

    move plotting code to python from notebook

--- qpga/plotting.py
+++ qpga/plotting.py
@@ -1,6 +1,8 @@
+import matplotlib as mpl
 import matplotlib.pyplot as plt
 import numpy as np
 import qutip
+from mpl_toolkits.axes_grid1 import make_axes_locatable
 
 from qpga.utils import reshape_state_vector
 
@@ -27,3 +29,116 @@ def plot_state_comparison(true_state, pred_state, iteration = None, savefig = Fa
         plt.close()
     else:
         plt.show()
+
+
+def _blob(x, y, w, w_min, w_max, area, cmap = None, ax = None):
+    """
+    Draws a square-shaped blob with the given area (< 1) at
+    the given coordinates.
+    """
+    hs = np.sqrt(area) / 2
+    xcorners = np.array([x - hs, x + hs, x + hs, x - hs])
+    ycorners = np.array([y - hs, y - hs, y + hs, y + hs])
+
+    handle = ax if ax is not None else plt
+    # color = int(256 * (w - w_min) / (w_max - w_min))
+    color = (w - w_min) / (w_max - w_min)
+    handle.fill(xcorners, ycorners, color = cmap(color))
+
+
+def computational_basis_labels(num_qubits, include_bras = True):
+    """Creates plot labels for matrix elements in the computational basis."""
+    N = 2 ** num_qubits
+    basis_labels = [format(i, 'b').zfill(num_qubits) for i in range(N)]
+
+    kets = [r"$\left|{}\right>$".format(l) for l in basis_labels]
+    if include_bras:
+        bras = [r"$\left<{}\right|$".format(l) for l in basis_labels]
+        return [kets, bras]
+    else:
+        return kets
+
+
+def hinton(W, xlabels = None, ylabels = None, title = None, fig = None, ax = None, cmap = None):
+    #     cmap = plt.get_cmap('hsv')
+    cmap = plt.get_cmap('twilight')
+
+    if ax is None:
+        fig, ax = plt.subplots(1, 1, figsize = (4, 4))
+
+    if not (xlabels or ylabels):
+        ax.axis('off')
+
+    ax.axis('equal')
+    ax.set_frame_on(False)
+
+    height, width = W.shape
+    ax.set(xlim = (0, width), ylim = (0, height))
+
+    max_abs = np.max(np.abs(W))
+    scale = 0.7
+
+    for i in range(width):
+        for j in range(height):
+            x = i + 1 - 0.5
+            y = j + 1 - 0.5
+            _blob(x, height - y, np.angle(W[i, j]), -np.pi, np.pi,
+                  np.abs(W[i, j]) / max_abs * scale, cmap = cmap, ax = ax)
+
+    # x axis
+    ax.xaxis.set_major_locator(plt.IndexLocator(1, 0.5))
+    if xlabels:
+        ax.set_xticklabels(xlabels, rotation = 'vertical')
+        ax.xaxis.tick_top()
+    ax.tick_params(axis = 'x', labelsize = 9, pad = 0)
+    ax.xaxis.set_ticks_position('none')
+
+    # y axis
+    ax.yaxis.set_major_locator(plt.IndexLocator(1, 0.5))
+    ax.yaxis.set_ticks_position('none')
+    if ylabels:
+        ax.set_yticklabels(list(reversed(ylabels)))
+    ax.tick_params(axis = 'y', labelsize = 9, pad = 0)
+
+    # color axis
+    divider = make_axes_locatable(ax)
+    cax = divider.append_axes('right', size = '4%', pad = '2%')
+    cbar = mpl.colorbar.ColorbarBase(cax, cmap = cmap,
+                                     norm = mpl.colors.Normalize(-np.pi, np.pi),
+                                     ticks = [])
+    #                                      ticks=[-np.pi, 0, np.pi])
+    cax.text(0.5, 0.0, '$-\pi$', transform = cax.transAxes, va = 'top', ha = 'center')
+    cax.text(0.5, 1.0, '$+\pi$', transform = cax.transAxes, va = 'bottom', ha = 'center')
+    #     cbar.ax.set_yticklabels(['$-\pi$','$0$','$+\pi$'])
+
+    # Make title in corner
+    if title is not None:
+        plt.text(-.05, 1.05, title, ha = 'center', va = 'center', fontsize = 22, transform = ax.transAxes)
+
+    return fig, ax
+
+
+def loss_plot(loss_val, loss_train, x_units = "epochs", x_max = None, fig = None, ax = None):
+    if ax is None:
+        fig, ax = plt.subplots(1, 1, figsize = (4, 4))
+
+    ax.plot(loss_val, linestyle = '-', label = "Validation")
+    ax.fill_between(np.arange(len(loss_val)), loss_val, alpha = 0.1)
+
+    ax.plot(loss_train, linestyle = ':', label = "Training")
+
+    ax.legend(loc = 'upper left')
+    ax.set_xlim(0, x_max - 1)
+    ax.set_ylim(0, 1)
+    ax.set_ylabel("Fidelity $\mathcal{F} = \left< \psi \\right| \\tilde{U}^{\\dagger} \hat{U} \left| \psi \\right>$")
+
+    if x_units == 'epochs':
+        ax.xaxis.set_major_locator(mpl.ticker.MaxNLocator(integer = True))
+        ax.set_xlabel("Epoch")
+    elif x_units == 'iterations':
+        ax.xaxis.set_major_locator(mpl.ticker.MaxNLocator(integer = True))
+        ax.set_xlabel("Iteration")
+    elif x_units == 'none':
+        ax.set_xticks([])
+
+    return fig, ax
